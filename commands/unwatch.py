4e645f15afb95f6a909e9a99b060043dac3146da
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-22 13:40:08 +0200

    update /unwatch to scenic oxygen 1.0

--- commands/unwatch.py
+++ commands/unwatch.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-from commands.watch import unwatch
+import main
+unwatch = main.load_code_as_module('watch').unwatch
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     unwatch(bot, chat_id, message)
--- commands/unwatchgifs.py
+++ commands/unwatchgifs.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-from commands.watchgifs import unwatch
+import main
+unwatch = main.load_code_as_module('watchgifs').unwatch
 
 def run(bot, chat_id, user='Dave', keyConfig=None, message='', totalResults=1):
     unwatch(bot, chat_id)
