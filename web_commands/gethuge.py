f8a5ae9486c4139ce62861bae4384fe915aeec22
Ashley Lewis <ultrakiff@gmail.com>
2018-05-25 10:38:40 +0200

    updating web commands

--- web_commands/gethuge.py
+++ web_commands/gethuge.py
@@ -4,7 +4,7 @@ get = main.get_platform_command_code('telegram', 'get')
 
 
 def run(keyConfig, message, totalResults=1):
-    requestText = message.encode('utf-8').replace(bot.name, "").strip()
+    requestText = message.encode('utf-8').strip()
     args = {'cx': keyConfig.get('Google', 'GCSE_IMAGE_SE_ID1'),
             'key': keyConfig.get('Google', 'GCSE_APP_ID'),
             'searchType': "image",
