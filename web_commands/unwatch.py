024d4b0fbad838c739ffd5098dae683a1e2d6185
Ashley Lewis <ultrakiff@gmail.com>
2018-05-25 11:24:59 +0200

    Delete unwatch.py

--- web_commands/unwatch.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import main
-unwatch = main.get_platform_command_code('telegram', 'watch').unwatch
-
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    unwatch(bot, chat_id, message)
-
-
