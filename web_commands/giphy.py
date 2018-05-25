3c5384ef0854fb30cc2543432e2a922e9c2de561
Ashley Lewis <ultrakiff@gmail.com>
2018-05-25 11:24:49 +0200

    Update web /giphy command

--- web_commands/giphy.py
+++ web_commands/giphy.py
@@ -7,8 +7,8 @@ import urllib
 import telegram
 
 
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    requestText = str(message).replace(bot.name, "").strip()
+def run(keyConfig, message, totalResults=1):
+    requestText = str(message).strip()
 
     giphyUrl = 'http://api.giphy.com/v1/gifs/search?q='
     apiKey = '&api_key=dc6zaTOxFJmzC&limit=10&offset=0'
@@ -16,14 +16,8 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     data = json.load(urllib.urlopen(realUrl))
     if data['pagination']['total_count'] >= 1:
         imagelink = data['data'][random.randint(0, len(data['data']) - 1)]['images']['original']['url']
-        bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_DOCUMENT)
-        bot.sendDocument(chat_id=chat_id,
-                         filename=str(requestText) + '.gif',
-                         document=str(imagelink))
         return str(imagelink)
     else:
-        errorMsg = 'I\'m sorry ' + (user if not user == '' else 'Dave') + \
-                   ', I\'m afraid I can\'t find a giphy gif for ' + \
+        errorMsg = 'I\'m sorry Dave, I\'m afraid I can\'t find a giphy gif for ' + \
                    string.capwords(requestText.encode('utf-8')) + '.'
-        bot.sendMessage(chat_id=chat_id, text=errorMsg)
         return [errorMsg]
