3d98db5929ddf4a54d10afeff48eb6810bc0f06d
Ashley Lewis <ultrakiff@gmail.com>
2018-05-25 10:38:07 +0200

    updating web commands

--- web_commands/getxxlarge.py
+++ web_commands/getxxlarge.py
@@ -3,14 +3,14 @@ import main
 get = main.get_platform_command_code('telegram', 'get')
 
 
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    requestText = message.replace(bot.name, "").strip()
+def run(keyConfig, message, totalResults=1):
+    requestText = message.strip()
     args = {'cx': keyConfig.get('Google', 'GCSE_IMAGE_SE_ID1'),
             'key': keyConfig.get('Google', 'GCSE_APP_ID'),
             'searchType': "image",
             'safe': "off",
             'q': requestText,
             "imgSize": "xxlarge"}
-    return get.Send_Images(bot, chat_id, user, requestText, args, keyConfig, totalResults)
+    return get.Send_Images(requestText, args, keyConfig, totalResults)
 
 
