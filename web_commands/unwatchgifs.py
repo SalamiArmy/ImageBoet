80507a0cbc97d05e00e1c6e535b90273aa1d23e8
Ashley Lewis <ultrakiff@gmail.com>
2018-05-25 11:25:15 +0200

    Delete unwatchgifs.py

--- web_commands/unwatchgifs.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import main
-unwatch = main.get_platform_command_code('telegram', 'watchgifs').unwatch
-
-def run(bot, chat_id, user='Dave', keyConfig=None, message='', totalResults=1):
-    unwatch(bot, chat_id)
-
-
