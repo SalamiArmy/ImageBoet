29c7065202f6d91bb34dfddbe2a26f129c4f5f87
Ashley Lewis <ultrakiff@gmail.com>
2018-05-25 10:35:14 +0200

    still updating web commands one command at a time

--- web_commands/gethugegif.py
+++ web_commands/gethugegif.py
@@ -3,7 +3,7 @@ import main
 getgif = main.get_platform_command_code('telegram', 'getgif')
 
 
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
+def run(keyConfig, message, totalResults=1):
     requestText = message.replace(bot.name, "").strip()
     args = {'cx': keyConfig.get('Google', 'GCSE_GIF_SE_ID1'),
             'key': keyConfig.get('Google', 'GCSE_APP_ID'),
@@ -13,4 +13,4 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
             'fileType': 'gif',
             'start': 1,
             'imgSize': 'huge'}
-    return getgif.Send_Animated_Gifs(bot, chat_id, user, requestText, args, keyConfig, totalResults)
+    return getgif.Send_Animated_Gifs(requestText, args, keyConfig, totalResults)
