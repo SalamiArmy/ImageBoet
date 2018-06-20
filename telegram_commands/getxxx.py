2fdcbcb7e98c680435cdee283525c07af633d575
Ashley Lewis <ultrakiff@gmail.com>
2018-06-20 08:02:51 +0200

    add to /getxxx filter

--- telegram_commands/getxxx.py
+++ telegram_commands/getxxx.py
@@ -102,6 +102,7 @@ def is_valid_xxx(xlink):
            'xhamster.com/user/' not in xlink and \
            'xhamster.com/search?q=' not in xlink and \
            'xhamster.com/search/photos?q=' not in xlink and \
+           'xhamster.com/photos/categories/' not in xlink and \
            'redtube.com/pornstar/' not in xlink and \
            'redtube.com/?search=' not in xlink and \
            'motherless.com/term/' not in xlink and \
