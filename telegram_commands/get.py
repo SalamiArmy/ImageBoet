c205825a9157b5cac79d48e8ed7ef2760cf9432f
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-10 11:27:48 +0200

    further reduce /get and /getgif search results walker limit down to 60

--- telegram_commands/get.py
+++ telegram_commands/get.py
@@ -279,7 +279,7 @@ def search_results_walker(args, bot, chat_id, data, number, requestText, results
                           (str(len(total_sent) + 1) + ' of ' + str(number) + '\n' if int(number) > 1 else '') + imagelink
                 bot.sendMessage(chat_id, message)
                 total_sent.append(imagelink)
-    if len(total_sent) < int(number) and int(total_offset) < int(total_results) and int(total_offset) < 100:
+    if len(total_sent) < int(number) and int(total_offset) < int(total_results) and int(total_offset) < 60:
         bot.sendMessage(chat_id=chat_id, text=
                         'I\'m sorry, search is taking longer because I\'m looking even harder now.')
         args['start'] = total_offset + 1
