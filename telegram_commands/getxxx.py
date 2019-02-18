6a76216f7055c75c74d987484588b845a77486b7
Ashley Lewis <ultrakiff@gmail.com>
2019-02-18 15:23:14 +0200

    filter out the orgasm blog

--- telegram_commands/getxxx.py
+++ telegram_commands/getxxx.py
@@ -112,7 +112,8 @@ def is_valid_xxx(xlink):
            'youporn.com/porntags/' not in xlink and \
            'eporner.com/photo/' not in xlink and \
            'eporner.com/search/' not in xlink and \
-           'eporner.com/search-photos/' not in xlink
+           'eporner.com/search-photos/' not in xlink and \
+           'orgasm.com/free-porn-blog/' not in xlink
 
 
 def Send_XXXs(bot, chat_id, user, requestText, data, total_results, results_this_page, number, args):
