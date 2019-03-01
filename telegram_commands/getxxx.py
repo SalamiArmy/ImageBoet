adfe9f1a7824217d67554c3317c0a4edc063c4f9
Ashley Lewis <ultrakiff@gmail.com>
2019-03-01 09:40:21 +0200

    update xhamster /getxxx filter

--- telegram_commands/getxxx.py
+++ telegram_commands/getxxx.py
@@ -107,12 +107,12 @@ def is_valid_xxx(xlink):
            'xhamster.com/stories/' not in xlink and \
            'xhamster.com/search/stories?q=' not in xlink and \
            'xhamster.com/user/' not in xlink and \
-           'xhamster.com/search?q=' not in xlink and \
            'xhamster.com/search/photos?q=' not in xlink and \
            'xhamster.com/photos/categories/' not in xlink and \
            'xhamster.com/blog/' not in xlink and \
            'xhamster.com/search/photos' not in xlink and \
            'xhamster.com/tags/' not in xlink and \
+           'xhamster.com/search?' not in xlink and \
            'redtube.com/pornstar/' not in xlink and \
            'redtube.com/?search=' not in xlink and \
            'motherless.com/term/' not in xlink and \
