09d096ea93b32ecced3914adb74bcd6c84e759d2
Ashley Lewis <ultrakiff@gmail.com>
2019-03-01 09:13:56 +0200

    use /get fillers in /getgif

--- telegram_commands/getgif.py
+++ telegram_commands/getgif.py
@@ -10,12 +10,6 @@ from PIL import Image
 
 CommandName = 'getgif'
 
-Fillers = ['Um, just need a sec...',
-           'Uh huh, I think I just need more time before I can get back to you.',
-           'Hmm, I have lots of results for you, but you\'ve seen all of them already.',
-           'Huh, none of these results are suitable for you. I\'m going to keep looking!',
-           'OK, I think I\'ve almost found a good one for you.']
-
 retry_on_telegram_error = main.get_platform_command_code('telegram', 'retry_on_telegram_error')
 get = main.get_platform_command_code('telegram', 'get')
 
@@ -113,7 +107,7 @@ def search_results_walker(args, bot, chat_id, data, number, requestText, results
                 bot.sendMessage(chat_id, message)
                 total_sent.append(imagelink)
     if len(total_sent) < int(number) and int(total_offset) < int(total_results) and int(total_offset) < 30:
-        bot.sendMessage(chat_id=chat_id, text=random.choice(Fillers).replace('[[USERNAME]]', user if not user == '' else 'Dave'))
+        bot.sendMessage(chat_id=chat_id, text=random.choice(get.Fillers).replace('[[USERNAME]]', user if not user == '' else 'Dave'))
         args['start'] = total_offset + 1
         data, total_results, results_this_page = get.Google_Custom_Search(args)
         return search_results_walker(args, bot, chat_id, data, number, requestText, results_this_page, total_results, keyConfig, user,
