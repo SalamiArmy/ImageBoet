0f2e35ad2bed025f46411a412fab22ce4575174f
Ashley Lewis <ultrakiff@gmail.com>
2019-06-13 12:43:23 +0200

    filter youporn/category out of /getxxx

--- telegram_commands/getxxx.py
+++ telegram_commands/getxxx.py
@@ -121,6 +121,7 @@ def is_valid_xxx(xlink):
            not xlink.endswith('/replies') and \
            'search?search=' not in xlink and \
            'youporn.com/porntags/' not in xlink and \
+           'youporn.com/category/' not in xlink and \
            'eporner.com/photo/' not in xlink and \
            'eporner.com/search/' not in xlink and \
            'eporner.com/search-photos/' not in xlink and \
