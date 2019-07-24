44a4a1455a1b2260c80a77e6beb8e416cdc534e3
Ashley Lewis <ultrakiff@gmail.com>
2019-07-24 14:25:24 +0200

    completing partial revert of my recent changes to /gettweet

--- telegram_commands/gettweet.py
+++ telegram_commands/gettweet.py
@@ -30,12 +30,11 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     setTwitterToken(chat_id, requestText)
   raw_data = urlfetch.fetch(url='https://api.twitter.com/1.1/search/tweets.json?q=' + requestText,
             headers={'Authorization': 'Bearer ' + getToken})
-  data = json.loads(raw_data.content)
+  getContent = raw_data.content
+  print(getContent)
+  data = json.loads()
   if ('statuses' in data and len(data['statuses']) > 0):
-    bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
-                                                      data['statuses'][0]['text'])
-    setTwitterToken(chat_id, "")
-    return True
+    bot.sendMessage(chat_id=chat_id, text=data['statuses'][0]['text'])
   else:
     bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                       ', I\'m afraid I can\'t find any Twitter Tweets for ' +
