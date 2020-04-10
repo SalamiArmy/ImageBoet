62ca58fe2ef8b18c5ad82674840998553d21ce99
Ashley Lewis <ultrakiff@gmail.com>
2020-04-10 11:26:03 +0200

    Can ignore xhamster listings

--- telegram_commands/getxxx.py
+++ telegram_commands/getxxx.py
@@ -159,7 +159,8 @@ def is_valid_xxx(xlink):
            'xnxx.com/pornstar/' not in xlink and \
            'xvideos.com/models/' not in xlink and \
            'tube8.com/cat/' not in xlink and \
-           'tube8.com/searches.html?q=' not in xlink
+           'tube8.com/searches.html?q=' not in xlink and \
+           'xhamster.com/pornstars/' not in xlink
 
 
 def Send_XXXs(bot, chat_id, user, requestText, data, total_results, results_this_page, number, args):
