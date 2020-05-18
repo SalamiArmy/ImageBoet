dea9c198a70eaa0a642962bb8d48667170d6c850
Ashley Lewis <ultrakiff@gmail.com>
2020-05-18 16:22:48 +0200

    add no token help message to /settweet

--- telegram_commands/settweet.py
+++ telegram_commands/settweet.py
@@ -1,4 +1,3 @@
-
 # coding=utf-8
 import sys
 
@@ -9,5 +8,8 @@ gettweet = main.get_platform_command_code('telegram', 'gettweet')
     
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = str(message).replace(bot.name, "").strip()
-    gettweet.setTwitterToken(chat_id, requestText)
-    bot.sendMessage(chat_id=chat_id, text='Twitter token set to:' + requestText)
+    if (requestText == ""):
+        bot.sendMessage(chat_id=chat_id, text='Set Twitter bearer token like this:\n/settweet AAAAAAAAAAAAAAAAAAAAAFlUwQAAAAAAct%2B... etc.\nSee https://developer.twitter.com/en/docs/basics/authentication/oauth-2-0/bearer-tokens' + requestText)
+    else:
+        gettweet.setTwitterToken(chat_id, requestText)
+        bot.sendMessage(chat_id=chat_id, text='Twitter token set to:' + requestText)
