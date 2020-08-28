e5602e1d71a9ea77a16277de21859a51e03cae84
Ashley Lewis <ultrakiff@gmail.com>
2020-08-28 17:11:03 +0200

    initial implementation of /leave

--- telegram_commands/leave.py
+++ telegram_commands/leave.py
@@ -1,1 +1,8 @@
+# coding=utf-8
+import main
+get = main.get_platform_command_code('telegram', 'get')
 
+
+def run(bot, chat_id, user, keyConfig, message, totalResults=1):
+    requestText = message.encode('utf-8').replace(bot.name, "").strip()
+    return bot.leaveChat(int(requestText))
