9c97df902551457a83504e9fffe47e75e631783a
Ashley Lewis <ultrakiff@gmail.com>
2021-09-29 17:27:44 +0200

    still trying to fix this fucking TypeError

--- telegram_commands/getxxx.py
+++ telegram_commands/getxxx.py
@@ -203,9 +203,8 @@ def Send_XXXs(bot, chat_id, user, requestText, data, total_results, results_this
             if len(total_sent) < int(number) and int(total_offset) < int(total_results):
                 args['start'] = total_offset+1
                 data, total_results, results_this_page = get.Google_Custom_Search(args)
-        if len(total_sent) < int(number):
-            bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
-                                                  ', I\'m afraid I cannot find enough filth for ' + requestText + '.')
+        if len(total_sent) >= int(number):
+            bot.sendMessage(chat_id=chat_id, text='I\'m sorry, I have failed')
         return total_sent
     else:
         errorMsg = 'I\'m sorry ' + (user if not user == '' else 'Dave') +\
